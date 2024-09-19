e3766e3ea2bb692e3bed828beab6aed58869245a
Juan Arango Renteria <91563685+JPArangoRenteria@users.noreply.github.com>
2024-09-19 10:14:16 -0400

    Update walker_constellation.m
    Will hole simulation event here, working on getting a working topology before overhauling it

--- walker_constellation.m
+++ walker_constellation.m
@@ -1,6 +1,7 @@
 function sc = walker_constellation(start_y, start_mon, start_d, start_h, start_min, start_s, ...
                                    stop_y, stop_mon, stop_d, stop_h, stop_min, stop_s, stepping_u, ...
-                                   numSatellites, numPlanes, relativeSpacing, altitude, inclination)
+                                   numSatellites, numPlanes, relativeSpacing, altitude, inclination, ...
+                                   walker_type)
     % Earth's radius in kilometers
     EarthRadius = 6378.14; % in kilometers
 
@@ -14,17 +15,17 @@ function sc = walker_constellation(start_y, start_mon, start_d, start_h, start_m
     scenario = satelliteScenario(startTime, stopTime, stepping_u);
 
     % Generate Walker constellation based on inclination
-    if inclination < 90
+    if strcmp(walker_type,'delta') == 1
         % Use walkerDelta for non-polar orbits (inclination < 90 degrees)
         sc = walkerDelta(scenario,radius,inclination,numSatellites,numPlanes,relativeSpacing);
-    elseif inclination == 90
+    elseif strcmp(walker_type,'star') == 1
         % Use walkerStar for polar orbits (inclination = 90 degrees)
         sc = walkerStar(scenario,radius,inclination,numSatellites,numPlanes,relativeSpacing);
     else
         % walkerDelta for retrograde orbits (inclination > 90 degrees)
-        sc = walkerDelta(scenario,radius,inclination,numSatellites,numPlanes,relativeSpacing);
+        sc = walkerStar(scenario,radius,inclination,numSatellites,numPlanes,relativeSpacing);
     end
     
     % Visualize the satellite scenario
-    satelliteScenarioViewer(scenario);
+    %satelliteScenarioViewer(scenario);
 end
