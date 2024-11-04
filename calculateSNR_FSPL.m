2bf43d117d23f05d7639dcf6c2bc4234891a9f04
benjaminvine <benjamin.r.vine@gmail.com>
2024-11-04 17:03:34 -0500

    Calculates SNR and FSPL
    Outputs SNR, FSPL, and timestamp arrays

---
+++ calculateSNR_FSPL.m
@@ -0,0 +1,76 @@
+% SNR and FSPL Function
+function [snrResults,fsplResults,timeSamples] = calculateSNR_FSPL(gsORsat,sat,cfg,startTime,duration,sampleTime)
+
+    % Pre-allocate results for FSPL and time
+    endTime = startTime + seconds(duration); % Total simulation time (in sec)
+    numSamples = floor(duration / sampleTime); % Number of samples taken
+    snrResults = zeros(numSamples, 0);
+    fsplResults = zeros(numSamples, 0);
+    timeSamples = datetime.empty(numSamples, 0);
+    timeSamples.TimeZone = 'UTC';
+
+    % Check gsORsat Object type
+    if isa(gsORsat,'matlabshared.satellitescenario.GroundStation')
+        % Iterate over each time step to compute FSPL
+        for i = 1:numSamples
+        
+            % Compute the current simulation time
+            currentTime = startTime + seconds((i-1) * sampleTime);
+        
+            % Error check for valid currentTime
+            if currentTime > endTime
+                currentTime = endTime;
+            end
+        
+            % Get the satellite's position (geographic / ECEF coordinates)
+            [satPos, ~] = states(sat, currentTime, 'CoordinateFrame', 'geographic');
+            [satECEF, ~] = states(sat, currentTime, 'CoordinateFrame', 'ecef');
+            satPosECEF = [satECEF(1) satECEF(2) satECEF(3)]; % Satellite ECEF coordinates
+    
+            % Get the ground stations position
+            gsPos = [gsORsat.Latitude gsORsat.Longitude gsORsat.Altitude];
+    
+            % Calculate the Elevation angle between ground station and satellite
+            maskAngle = 15;                   % Set mask angle to 5 degrees
+            [az,el,vis] = lookangles(gsPos,satPosECEF,maskAngle);
+    
+            % Calculate the distance between the satellite and the ground station
+            d = slantRangeCircularOrbit(el,satPos(3),gsPos(3)); % in meters
+            cfg.Distance = d/1000;
+    
+            % Compute FSPL using the distance and frequency
+            % pathLoss = fspl(d,wl); %FSPL in dB
+            [cn,info] = satelliteCNR(cfg);
+            snrResults(i) = cn;
+            fsplResults(i) = info.FSPL ;
+            timeSamples(i) = currentTime; % Save current time
+        end
+    else
+        % Iterate over each time step to compute FSPL
+        for i = 1:numSamples
+        
+            % Compute the current simulation time
+            currentTime = startTime + seconds((i-1) * sampleTime);
+        
+            % Error check for valid currentTime
+            if currentTime > endTime
+                currentTime = endTime;
+            end
+        
+            % Get the satellite's position (geographic / ECEF coordinates)
+            [sat1Pos, ~] = states(sat, currentTime, 'CoordinateFrame', 'geographic');
+            [sat2Pos, ~] = states(gsORsat, currentTime, 'CoordinateFrame', 'geographic');
+
+            % Calculate the distance between the two satellites
+            d = norm(sat1Pos - sat2Pos);
+            cfg.Distance = d/1000;
+    
+            % Compute SNR and FSPL using the satelliteCNRConfig object
+            [cn,info] = satelliteCNR(cfg);
+            snrResults(i) = cn;             % Save SNR result
+            fsplResults(i) = info.FSPL ;    % Save FSPL result
+            timeSamples(i) = currentTime;   % Save current time
+        
+        end
+    end
+end
